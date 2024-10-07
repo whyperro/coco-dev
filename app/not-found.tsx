a5b240fc283d4c15d612e0b041f38999fffca642
Angel Antón <aanton@transmandu.com>
2024-10-07 13:08:17 -0400

    nuevos redirect dependiendo de usuario

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,17 @@
 import { Button } from '@/components/ui/button'
 import { MapPinned, Plane } from 'lucide-react'
+import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 
 export default function NotFound() {
+  const { data: session } = useSession()
   return (
     <div className='h-dvh w-dvw flex flex-col items-center justify-center'>
       <div className='flex flex-col justify-center items-center max-w-xl gap-y-4'>
         <MapPinned className='size-[110px] text-primary' />
         <h1 className='font-bold text-7xl text-center text-primary'>¡No Encontrado!</h1>
         <p className='text-lg text-muted-foreground text-center'>Lo sentimos, pero la página que estabas buscando no pudo ser localizada. Es posible que se haya movido o ya no exista.</p>
-        <Link href={'/dashboard'}>
+        <Link href={session?.user.user_role === 'ADMIN' || session?.user.user_role === 'AUDITOR' ? "/dashboard" : "/estadisticas"}>
           <Button className='bg-primary text-white hover:bg-transparent hover:text-black hover:border hover:border-black'>Volver</Button>
         </Link>
       </div>
--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -7,8 +7,10 @@ import { useSidebarToggle } from "@/hooks/useSidebarToggle";
 import { useStore } from "@/hooks/useStore";
 import { Menu } from "./Menu";
 import { SidebarToggle } from "./SidebarToggle";
+import { useSession } from "next-auth/react";
 
 export function Sidebar() {
+  const { data: session } = useSession();
   const sidebar = useStore(useSidebarToggle, (state) => state);
 
   if (!sidebar) return null;
@@ -30,11 +32,7 @@ export function Sidebar() {
           variant="link"
           asChild
         >
-          {/* <Link href="/dashboard" className="flex items-center gap-2 font-extrabold text-4xl text-[#63c144]">
-              Berkana
-            </Link> */}
-
-          <Link href="/dashboard" className="flex items-center gap-2">
+          <Link href={session?.user.user_role === 'ADMIN' || session?.user.user_role === 'AUDITOR' ? "/dashboard" : "/estadisticas"} className="flex items-center gap-2">
             <h1
               className={cn(
                 "text-[#63c144] font-extrabold text-5xl mt-8 whitespace-nowrap transition-[transform,opacity,display] ease-in-out duration-300 ",

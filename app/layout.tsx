48e86d09afd94617555502afe6edc4e2383ec26c
Angel Antón <aanton@transmandu.com>
2024-11-04 15:56:30 -0400

    devtools in prod

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,16 +6,26 @@ import QueryClientContextProvider from "@/providers/QueryProvider";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from "next-themes";
 import { Poppins } from "next/font/google";
-import { useEffect } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 import "./globals.css";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] });
+
+
+const ReactQueryDevtoolsProduction = lazy(() =>
+  import('@tanstack/react-query-devtools/build/modern/production.js').then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    }),
+  ),
+)
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
+  const [showDevtools, setShowDevtools] = useState(false)
   useEffect(() => {
     // @ts-expect-error: El módulo se carga dinámicamente y puede que no esté disponible en build
     window.toggleDevtools = () => setShowDevtools((old) => !old)
@@ -33,7 +43,12 @@ export default function RootLayout({
             </ThemeProvider>
           </body>
         </ClientSessionProvider>
-        <ReactQueryDevtools initialIsOpen={true} />
+        <ReactQueryDevtools initialIsOpen />
+        {showDevtools && (
+          <Suspense fallback={null}>
+            <ReactQueryDevtoolsProduction />
+          </Suspense>
+        )}
       </QueryClientContextProvider>
     </html>
   );

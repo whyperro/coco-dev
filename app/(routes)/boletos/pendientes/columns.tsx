8c92758d22a5de7257966e6719e4417932fa5abd
Angel Antón <aanton@transmandu.com>
2024-11-07 09:41:20 -0400

    mejora en el column de proveedores

--- app/(routes)/boletos/pendientes/columns.tsx
+++ app/(routes)/boletos/pendientes/columns.tsx
@@ -134,7 +134,6 @@ export const columns: ColumnDef<Ticket>[] = [
   {
     id: "actions",
     cell: ({ row }) => {
-      const id = row.original.id
       return (
         <PendingTicketsDropdownActions ticket={row.original} />
       )

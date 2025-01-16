ffa69c292abbd257f5c2370cf53c7f68ed86419a
Angel Antón <aanton@transmandu.com>
2025-01-16 13:17:09 -0400

    quitado revision de nro de ticket repetido

--- app/api/tickets/route.ts
+++ app/api/tickets/route.ts
@@ -4,26 +4,6 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
     try {
         const data = await request.json();
-        // Check if the branch name already exists
-
-        if(data.ticket_number !== undefined){
-          const ticketFound = await db.ticket.findFirst({
-            where: {
-              ticket_number: data.ticket_number,
-            },
-          });
-
-          if (ticketFound) {
-            return NextResponse.json(
-              {
-              message: "El ticket ya existe.",
-              },
-              {
-              status: 400, // Set 400 status code for a validation error
-              }
-            );
-          }
-        }
 
         const newTicket = await db.ticket.create({
           data: {
